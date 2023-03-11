e1cba37129fca7bf826a6e247637f72ebc0dc576
Yash Diwaker <190104076@hbtu.ac.in>
2023-03-12 03:10:55 +0530

    home done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react"
+import {BrowserRouter, Routes,Route} from "react-router-dom"
+import Header from "./component/layout/Header";
+import Home from "./component/home/Home";
+//import css
+import "./styles/app.scss"
+import "./styles/header.scss"
+import "./styles/home.scss"
+import "./styles/founder.scss"
+import "./styles/menu.scss"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
