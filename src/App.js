58cbb84bcaa41e7d55baa81a545362a252f1d7ed
Yash Diwaker <190104076@hbtu.ac.in>
2023-03-16 01:00:14 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,44 @@ import react from "react"
 import {BrowserRouter, Routes,Route} from "react-router-dom"
 import Header from "./component/layout/Header";
 import Home from "./component/home/Home";
-//import css
+import Contact from "./component/contact/Contact";
+import Cart from "./component/cart/Cart";
+import Shipping from "./component/cart/Shipping";
+import ConfirmOrder from "./component/cart/ConfirmOrder";
+import PaymentSuccess from "./component/cart/PaymentSuccess";
+import Login from "./component/login/Login"
+import Profile from "../src/component/profile/Profile.jsx"
+import MyOrders from "./component/myOrders/MyOrder.jsx"
+import OrderDetails from "./component/myOrders/OrderDetails.jsx"
+import DashBoard from "./component/admin/Dashboard.jsx"
+import Users from "./component/admin/Users.jsx"
+import Orders from "./component/admin/Orders.jsx"
+import NotFound from "./component/layout/NotFound";
+import About from "./component/about/About.jsx";
+
+
+//import scss
 import "./styles/app.scss"
 import "./styles/header.scss"
 import "./styles/home.scss"
 import "./styles/founder.scss"
 import "./styles/menu.scss"
-
+import "./styles/contact.scss"
+import "./styles/cart.scss"
+import "./styles/shipping.scss"
+import "./styles/confirmOrder.scss"
+import "./styles/paymentSuccess.scss"
+import "./styles/login.scss"
+import "./styles/profile.scss"
+import "./styles/table.scss"
+import "./styles/orderdetails.scss"
+import "./styles/dashboard.scss"
+import "./styles/users.scss"
+import "./styles/orders.scss"
+import "./styles/footer.scss"
+import "./styles/notfound.scss"
+import "./styles/about.scss"
+import Footer from "./component/layout/Footer";
 
 function App() {
   return (
@@ -16,7 +47,22 @@ function App() {
     <Header/>
     <Routes>
           <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/contact" element={<Contact/>}></Route>
+          <Route exact path="/cart" element={<Cart/>}></Route>
+          <Route exact path="/shipping" element={<Shipping/>}></Route>
+          <Route exact path="/confirmorder" element={<ConfirmOrder/>}></Route>
+          <Route exact path="/paymentSuccess" element={<PaymentSuccess/>}></Route>
+          <Route exact path="/login" element={<Login/>}></Route>
+          <Route exact path="/me" element={<Profile/>}></Route>
+          <Route exact path="/myorders" element={<MyOrders/>}></Route>
+          <Route exact path="/order/:id" element={<OrderDetails/>}></Route>
+          <Route exact path="/admin/dashboard" element={<DashBoard/>}></Route>
+          <Route exact path="/admin/users" element={<Users/>}></Route>
+          <Route exact path="/admin/orders" element={<Orders/>}></Route>
+          <Route exact path="about" element={<About/>}/>
+          <Route path="*" element={<NotFound />} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
   );
 }
--- src/component/home/Founder.jsx
+++ src/component/home/Founder.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import me from "../../assests/founder.webp";
+import me from "../../assests/WhatsApp Image 2023-03-14 at 2.52.09 AM.jpeg";
 const Founder = () => {
   const options = {
     initial: {
@@ -17,10 +17,10 @@ const Founder = () => {
     <section className="founder">
       <motion.div {...options}>
         <img src={me} alt="Founder" height={200} width={200} />
-        <h3>Abhishek Singh</h3>
+        <h3>Bhalu</h3>
 
         <p>
-          Hey, Everyone I am Abhishek Singh, the founder of MBA Burger Wala.
+          Hey, Everyone Bhalu Burger Wala, the founder of Bhalu Burger Wala.
           <br />
           Our aim is to create the most tasty burger on planet.
         </p>
